b6197d5dcfd01327945dd5eb7be4f6ed94f4bfd3
frankschaare <frank.schaare@gmail.com>
2014-03-18 13:00:50 +0100

    20140318: NavTree erweitert

--- de.hannit.fsch.common.organisation/src/de/hannit/fsch/common/organisation/IOrganisation.java
+++ de.hannit.fsch.common.organisation/src/de/hannit/fsch/common/organisation/IOrganisation.java
@@ -3,6 +3,11 @@
  */
 package de.hannit.fsch.common.organisation;
 
+import java.util.TreeMap;
+
+import de.hannit.fsch.common.mitarbeiter.Mitarbeiter;
+import de.hannit.fsch.common.mitarbeiter.Team;
+
 
 /**
  * @author fsch
@@ -11,5 +16,8 @@ package de.hannit.fsch.common.organisation;
 public interface IOrganisation
 {
 public String getName();
-
+public void setMitarbeiter(TreeMap<Integer, Mitarbeiter> mitarbeiter);
+public TreeMap<String, Mitarbeiter> getMitarbeiterNachName();
+public TreeMap<Integer, Mitarbeiter> getMitarbeiterNachPNR();
+public TreeMap<Integer, Team> getTeams();
 }
